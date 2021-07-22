afbc927846f635bdc1be00ed1180762a3e7f87b2
predatory123 <13310948808@163.com>
2021-07-22 22:55:03 +0800

    insert: 文件更新

--- readtest/src/main/java/SchroedingerProgrammiertJava/src/de/galileocomputing/schroedinger/java/kapitel18/cocktails/zutaten/Milch.java
+++
@@ -1,9 +0,0 @@
-package SchroedingerProgrammiertJava.src.de.galileocomputing.schroedinger.java.kapitel18.cocktails.zutaten;
-
-/**
- * 
- * @author Philip Ackermann
- *
- */
-public class Milch implements Zutat {
-}
