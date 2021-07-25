8bcf397d816f5eb3b09ba22684da18a39a670a6e
predatory123 <13310948808@163.com>
2021-07-25 17:06:30 +0800

    new case samples

---
+++ readtest/src/main/java/thinking/in/java/chapter04/DefaultConstructor.java
@@ -0,0 +1,14 @@
+package thinking.in.java.chapter04;
+
+/**
+ * @author: wenyongjie
+ * @date: 2021/7/25 17:05
+ */
+class Brid {
+    int i;
+}
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        Brid nc = new Brid();
+    }
+}
